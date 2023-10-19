84f4dd0998b3de6debf10d5ec5542b3d7c0e0a86
xiwangly2 <1334850101@qq.com>
2023-10-19 22:16:50 +0800

    最基本的注册模板

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
+use http\Client\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('home');
+//});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*// 注册
+Route::get('/register', [RegisterController::class])->name('register');
+
+// 登录
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class => 'login'])->name('login');
+
+
+// 显示所有文章
+Route::get('/articles', 'ArticleController@index');
+
+// 显示单个文章
+Route::get('/articles/{article}', 'ArticleController@show');
+
+// 创建文章表单
+Route::get('/articles/create', 'ArticleController@create');
+
+// 保存新文章
+Route::post('/articles', 'ArticleController@store');
+
+// 编辑文章表单
+Route::get('/articles/{article}/edit', 'ArticleController@edit');
+
+// 更新文章
+Route::put('/articles/{article}', 'ArticleController@update');
+
+// 删除文章
+Route::delete('/articles/{article}', 'ArticleController@destroy');
+*/
